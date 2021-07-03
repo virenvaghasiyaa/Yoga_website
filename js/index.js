eb9e32195402640cde487af8d7452e0a656ecca2
viren vaghasiya <78996487+virenvaghasiyaa@users.noreply.github.com>
2021-07-03 22:27:59 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -54,12 +54,26 @@ $('.testimonials').slick({
     arrows:false,
     centerMode: true,
     centerpadding: '200px',
+    autoplay: true,
+    autoplayspeed: 1000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
